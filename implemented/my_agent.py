9dafb27b1317ba4f39237bc788f255a4b6ad12ef
khodozzz <khodozzz@gmail.com>
2022-06-01 16:33:53 +0300

    update demo

--- implemented/my_agent.py
+++ implemented/my_agent.py
@@ -5,11 +5,18 @@ from base.agent.perception.perception import Perception
 from base.agent.thinking.thinking import Thinking
 
 
-class DummyThinking(Thinking):
-    def __init__(self, agent):
+class MyWorldModel:
+    time = None
+
+
+class MyThinking(Thinking):
+    def __init__(self, agent, wm):
         Thinking.__init__(self, agent)
 
-        self.is_moved = False
+        self.wm = wm
+
+        self.clockwise = True
+        self.moved = False
 
     def think(self):
         self.agent.sense_body_event.clear()
@@ -18,19 +25,35 @@ class DummyThinking(Thinking):
         self.agent.see_event.clear()
         self.agent.see_event.wait()
 
-        if not self.is_moved:
+        if self.wm.time % 10 == 1:
+            self.clockwise = not self.clockwise
+
+        if not self.moved:
             self.agent.execute_command(Command('move', -5, 0))
             self.agent.execute_command(Command('change_view', 'narrow', 'high'))
-            self.is_moved = True
-        else:
+            self.moved = True
+        elif self.clockwise:
             self.agent.execute_command(Command('turn', 45))
+        else:
+            self.agent.execute_command(Command('turn', -45))
+
+
+class MyPerception(Perception):
+    def __init__(self, agent, wm):
+        Perception.__init__(self, agent)
+        self.wm = wm
+
+    def handle_sense_body(self, msg_time, sense_body):
+        self.wm.time = msg_time
 
 
 if __name__ == '__main__':
     agent = Agent('LETIgers')
-    agent.perception = Perception(agent)
     agent.execution = Execution(agent)
 
-    agent.thinking = DummyThinking(agent)
+    wm = MyWorldModel()
+
+    agent.perception = MyPerception(agent, wm)
+    agent.thinking = MyThinking(agent, wm)
 
     agent.run()
